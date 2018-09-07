003a820985a1d4d8a37e184725bc0a944be126a3
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-09-07 13:21:34 -0700

    Adds autocomplete, or something

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 (setq load-prefer-newer t)
 ;; initialize the packages and create the packages list if not exists
 (package-initialize)
+
 (when (not package-archive-contents)
   (package-refresh-contents))
 
@@ -21,45 +22,68 @@
 
 (unless (package-installed-p 'rainbow-delimiters)
   (package-install 'rainbow-delimiters))
+
 (unless (package-installed-p 'hydra)
   (package-install 'hydra))
+
 (unless (package-installed-p 'restart-emacs)
   (package-install 'restart-emacs))
+
+(unless (package-installed-p 'helm)
+  (package-install 'helm))
 
 (eval-when-compile
   (require 'use-package)
   (require 'ibuffer))
 
-(use-package which-key)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Blackout - control the mode line
+(add-to-list 'load-path "~/src/blackout")
+(use-package blackout)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; which-key allows the lazy emacs user to start typing a key combination,
+;; pause, and then see what's actually possible
+(use-package which-key
+  :blackout t)
 (which-key-mode)
-(use-package rainbow-delimiters)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; yay for custom menus via hydra
 (use-package hydra)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Pretty parens and curly bois
+(use-package rainbow-delimiters)
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; restart emacs
 ;;
-;; Provides a way to quit or restart emacs, all from C-x C-c
+;; Provides a way to quit or restart emacs, bound to C-x r r
 (use-package restart-emacs)
+(define-key global-map (kbd "C-x r r") 'restart-emacs)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; helm
 (require 'helm)
 ;; enable fuzzy match
-(setq helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match    t
+(setq helm-mode-fuzzy-match                 t
+      helm-completion-in-region-fuzzy-match t
       helm-mini-default-sources '(helm-source-buffers-list
                                   helm-source-recentf
                                   helm-source-bookmarks
                                   helm-source-buffer-not-found))
 ;; some keybinds
-(global-set-key (kbd "M-x") 'nil)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x b") 'nil)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "\C-cfr") 'helm-recentf)
+(global-set-key (kbd "M-x")                          'nil)
+(global-set-key (kbd "M-x")                          'helm-M-x)
+(global-set-key (kbd "C-x b")                        'nil)
+(global-set-key (kbd "C-x b")                        'helm-mini)
+(global-set-key (kbd "\C-cfr")                       'helm-recentf)
 (global-set-key (kbd "M-y")                          'helm-show-kill-ring)
 (global-set-key (kbd "C-x C-f")                      'helm-find-files)
 (global-set-key (kbd "C-c <SPC>")                    'helm-all-mark-rings)
@@ -89,8 +113,139 @@
 (define-key global-map (kbd "M-g i")                 'helm-gid)
 (define-key global-map (kbd "C-x p h")               'helm-projects-history)
 ;(define-key global-map (kbd "C-x r c")               'helm-addressbook-bookmarks)
-(define-key global-map (kbd "C-x r r")               'restart-emacs)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Autocompletion
+;;
+;; Package `company' provides an in-buffer autocompletion framework.
+;; It allows for packages to define backends that supply completion
+;; candidates, as well as optional documentation and source code. Then
+;; Company allows for multiple frontends to display the candidates,
+;; such as a tooltip menu. Company stands for "Complete Anything".
+(use-package company
+  :defer 3
+  :init
+
+  (defvar radian--company-backends-global
+    '(company-capf
+      company-files
+      (company-dabbrev-code company-keywords)
+      company-dabbrev)
+    "Values for `company-backends' used everywhere.
+If `company-backends' is overridden by Radian, then these
+backends will still be included.")
+
+  :bind (;; Remap the standard Emacs keybindings for invoking
+         ;; completion to instead use Company. You might think this
+         ;; could be put in the `:bind*' declaration below, but it
+         ;; seems that `bind-key*' does not work with remappings.
+         ([remap completion-at-point] . company-manual-begin)
+         ([remap complete-symbol] . company-manual-begin)
+
+         ;; The following are keybindings that take effect whenever
+         ;; the completions menu is visible, even if the user has not
+         ;; explicitly interacted with Company.
+
+         :map company-active-map
+
+         ;; Make TAB always complete the current selection, instead of
+         ;; only completing a common prefix.
+         ("<tab>" . company-complete-selection)
+         ("TAB" . company-complete-selection)
+
+         ;; The following are keybindings that only take effect if the
+         ;; user has explicitly interacted with Company. Note that
+         ;; `:map' from above is "sticky", and applies also below: see
+         ;; https://github.com/jwiegley/use-package/issues/334#issuecomment-349473819.
+
+         :filter (company-explicit-action-p)
+
+         ;; Make RET trigger a completion if and only if the user has
+         ;; explicitly interacted with Company, instead of always
+         ;; doing so.
+         ("<return>" . company-complete-selection)
+         ("RET" . company-complete-selection)
+
+         ;; We then make <up> and <down> abort the completions menu
+         ;; unless the user has interacted explicitly. Note that we
+         ;; use `company-select-previous' instead of
+         ;; `company-select-previous-or-abort'. I think the former
+         ;; makes more sense since the general idea of this `company'
+         ;; configuration is to decide whether or not to steal
+         ;; keypresses based on whether the user has explicitly
+         ;; interacted with `company', not based on the number of
+         ;; candidates.
+         ;;
+         ;; Note that M-p and M-n work regardless of whether explicit
+         ;; interaction has happened yet, and note also that M-TAB
+         ;; when the completions menu is open counts as an
+         ;; interaction.
+         ("<up>" . company-select-previous)
+         ("<down>" . company-select-next))
+
+  :bind* (;; The default keybinding for `completion-at-point' and
+          ;; `complete-symbol' is M-TAB or equivalently C-M-i. We
+          ;; already remapped those bindings to `company-manual-begin'
+          ;; above. Here we make sure that they definitely invoke
+          ;; `company-manual-begin' even if a minor mode binds M-TAB
+          ;; directly.
+          ("M-TAB" . company-manual-begin))
+
+  :config
+
+  ;; Always display the entire suggestion list onscreen, placing it
+  ;; above the cursor if necessary.
+  (setq company-tooltip-minimum company-tooltip-limit)
+
+  ;; Always display suggestions in the tooltip, even if there is only
+  ;; one. Also, don't display metadata in the echo area. (This
+  ;; conflicts with ElDoc.)
+  (setq company-frontends '(company-pseudo-tooltip-frontend))
+
+  ;; Show quick-reference numbers in the tooltip. (Select a completion
+  ;; with M-1 through M-0.)
+  (setq company-show-numbers t)
+
+  ;; Prevent non-matching input (which will dismiss the completions
+  ;; menu), but only if the user interacts explicitly with Company.
+  (setq company-require-match #'company-explicit-action-p)
+
+  ;; Company appears to override our settings in `company-active-map'
+  ;; based on `company-auto-complete-chars'. Turning it off ensures we
+  ;; have full control.
+  (setq company-auto-complete-chars nil)
+
+  ;; Only search the current buffer to get suggestions for
+  ;; `company-dabbrev' (a backend that creates suggestions from text
+  ;; found in your buffers). This prevents Company from causing lag
+  ;; once you have a lot of buffers open.
+  (setq company-dabbrev-other-buffers nil)
+
+  ;; Make the `company-dabbrev' backend fully case-sensitive, to
+  ;; improve the UX when working with domain-specific words that have
+  ;; particular casing.
+  (setq company-dabbrev-ignore-case nil)
+  (setq company-dabbrev-downcase nil)
+
+  ;; When candidates in the autocompletion tooltip have additional
+  ;; metadata, like a type signature, align that information to the
+  ;; right-hand side. This usually makes it look neater.
+  (setq company-tooltip-align-annotations t)
+
+  (global-company-mode +1)
+
+  :blackout t)
+
+;; Package `company-prescient' provides intelligent sorting and
+;; filtering for candidates in Company completions.
+(use-package company-prescient
+  :demand t
+  :after company
+  :config
+
+  ;; Use `prescient' for Company menus.
+  (company-prescient-mode +1))
 
 ;(global-set-key "\C-cd" 'dash-at-point)
 ;(global-set-key "\C-ce" 'dash-at-point-with-docset)
@@ -726,7 +881,7 @@ _t_: toggle    _._: toggle hydra _H_: help       C-o other win no-select
     ("f71859eae71f7f795e734e6e7d178728525008a28c325913f564a42f74042c31" default)))
  '(package-selected-packages
    (quote
-    (restart-emacs helm-rg ace-window helm helm-bibtex org org-bullets org-journal org-plus-contrib pdf-tools which-key use-package challenger-deep-theme rainbow-delimiters))))
+    (company company-cabal company-lsp company-prescient restart-emacs helm-rg ace-window helm helm-bibtex org org-bullets org-journal org-plus-contrib pdf-tools which-key use-package challenger-deep-theme rainbow-delimiters hydra))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
