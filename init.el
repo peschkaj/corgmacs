c5203ab515aa1175ee624748d6975117cf823464
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-09-10 15:30:34 -0700

    Cleans up helm configurationg

--- init.el
+++ init.el
@@ -104,54 +104,61 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; helm
-(require 'helm)
+(use-package helm
+  :bind (:map helm-map
+              ("<tab>" . helm-execute-persistent-action))
+  :config
+  (require 'helm-config)
+  (setq helm-mode-fuzzy-match                 t ;; enable fuzzy match
+        helm-M-x-fuzzy-match                  t
+        helm-recentf-fuzzy-match              t
+        helm-buffers-fuzzy-matching           t
+        helm-ff-fuzzy-matching                t
+        helm-completion-in-region-fuzzy-match t
+        helm-mini-default-sources '(helm-source-buffers-list
+                                    helm-source-recentf
+                                    helm-source-bookmarks
+                                    helm-source-buffer-not-found))
+  ;; some keybinds
+  (global-set-key (kbd "M-x")                          'nil)
+  (global-set-key (kbd "M-x")                          'helm-M-x)
+  (global-set-key (kbd "C-x b")                        'nil)
+  (global-set-key (kbd "C-x b")                        'helm-mini)
+  (global-set-key (kbd "\C-cfr")                       'helm-recentf)
+  (global-set-key (kbd "M-y")                          'helm-show-kill-ring)
+  (global-set-key (kbd "C-x C-f")                      'helm-find-files)
+  (global-set-key (kbd "C-c <SPC>")                    'helm-all-mark-rings)
+  (global-set-key (kbd "C-x r b")                      'helm-filtered-bookmarks)
+  (global-set-key (kbd "C-:")                          'helm-eval-expression-with-eldoc)
+  (global-set-key (kbd "C-,")                          'helm-calcul-expression)
+  (global-set-key (kbd "C-h d")                        'helm-info-at-point)
+  (global-set-key (kbd "C-h i")                        'helm-info)
+  (global-set-key (kbd "C-x C-d")                      'helm-browse-project)
+  (global-set-key (kbd "<f1>")                         'helm-resume)
+  (global-set-key (kbd "C-h C-f")                      'helm-apropos)
+  (global-set-key (kbd "C-h a")                        'helm-apropos)
+  (global-set-key (kbd "C-h C-d")                      'helm-debug-open-last-log)
+  (global-set-key (kbd "<f5> s")                       'helm-find)
+  (global-set-key (kbd "S-<f2>")                       'helm-execute-kmacro)
+  (global-set-key (kbd "C-c i")                        'helm-imenu-in-all-buffers)
+  ;(global-set-key (kbd "<f11> o")                      'helm-org-agenda-files-headings)
+  (global-set-key (kbd "C-s")                          'helm-occur)
+  ;; (progn (define-key prog-mode-map (kbd "C-s") 'helm-occur)
+  ;;        (define-key text-mode-map (kbd "C-s") 'helm-occur))
+  (define-key global-map [remap jump-to-register]      'helm-register)
+  (define-key global-map [remap list-buffers]          'helm-mini)
+  (define-key global-map [remap dabbrev-expand]        'helm-dabbrev)
+  (define-key global-map [remap find-tag]              'helm-etags-select)
+  (define-key global-map [remap xref-find-definitions] 'helm-etags-select)
+  (define-key global-map (kbd "M-g a")                 'helm-do-grep-ag)
+  (define-key global-map (kbd "M-g g")                 'helm-grep-do-git-grep)
+  (define-key global-map (kbd "M-g r")                 'helm-rg)
+  (define-key global-map (kbd "M-g i")                 'helm-gid)
+  (define-key global-map (kbd "C-x p h")               'helm-projects-history)
+  (helm-mode 1)
+  :blackout t)
 (use-package helm-bibtex
   :ensure t)
-;; enable fuzzy match
-(setq helm-mode-fuzzy-match                 t
-      helm-completion-in-region-fuzzy-match t
-      helm-mini-default-sources '(helm-source-buffers-list
-                                  helm-source-recentf
-                                  helm-source-bookmarks
-                                  helm-source-buffer-not-found))
-;; some keybinds
-(global-set-key (kbd "M-x")                          'nil)
-(global-set-key (kbd "M-x")                          'helm-M-x)
-(global-set-key (kbd "C-x b")                        'nil)
-(global-set-key (kbd "C-x b")                        'helm-mini)
-(global-set-key (kbd "\C-cfr")                       'helm-recentf)
-(global-set-key (kbd "M-y")                          'helm-show-kill-ring)
-(global-set-key (kbd "C-x C-f")                      'helm-find-files)
-(global-set-key (kbd "C-c <SPC>")                    'helm-all-mark-rings)
-(global-set-key (kbd "C-x r b")                      'helm-filtered-bookmarks)
-(global-set-key (kbd "C-:")                          'helm-eval-expression-with-eldoc)
-(global-set-key (kbd "C-,")                          'helm-calcul-expression)
-(global-set-key (kbd "C-h d")                        'helm-info-at-point)
-(global-set-key (kbd "C-h i")                        'helm-info)
-(global-set-key (kbd "C-x C-d")                      'helm-browse-project)
-(global-set-key (kbd "<f1>")                         'helm-resume)
-(global-set-key (kbd "C-h C-f")                      'helm-apropos)
-(global-set-key (kbd "C-h a")                        'helm-apropos)
-(global-set-key (kbd "C-h C-d")                      'helm-debug-open-last-log)
-(global-set-key (kbd "<f5> s")                       'helm-find)
-(global-set-key (kbd "S-<f2>")                       'helm-execute-kmacro)
-(global-set-key (kbd "C-c i")                        'helm-imenu-in-all-buffers)
-;(global-set-key (kbd "<f11> o")                      'helm-org-agenda-files-headings)
-(global-set-key (kbd "C-s")                          'helm-occur)
-;; (progn (define-key prog-mode-map (kbd "C-s") 'helm-occur)
-;;        (define-key text-mode-map (kbd "C-s") 'helm-occur))
-
-(define-key global-map [remap jump-to-register]      'helm-register)
-(define-key global-map [remap list-buffers]          'helm-mini)
-(define-key global-map [remap dabbrev-expand]        'helm-dabbrev)
-(define-key global-map [remap find-tag]              'helm-etags-select)
-(define-key global-map [remap xref-find-definitions] 'helm-etags-select)
-(define-key global-map (kbd "M-g a")                 'helm-do-grep-ag)
-(define-key global-map (kbd "M-g g")                 'helm-grep-do-git-grep)
-(define-key global-map (kbd "M-g r")                 'helm-rg)
-(define-key global-map (kbd "M-g i")                 'helm-gid)
-(define-key global-map (kbd "C-x p h")               'helm-projects-history)
-;(define-key global-map (kbd "C-x r c")               'helm-addressbook-bookmarks)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -435,8 +442,9 @@ provide such a commit message."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Flycheck
 (use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
+  :ensure t)
+
+(add-hook 'prog-mode-hook #'flycheck-mode)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -688,7 +696,10 @@ _t_: toggle    _._: toggle hydra _H_: help       C-o other win no-select
       initial-scratch-message nil             ;; remove the message in the scratch buffer
       visible-bell t                          ;; remove the annoying beep
       apropos-do-all t                        ;; apropos commands perform more extensive searches than default
-      large-file-warning-threshold 100000000) ;; warn only when opening files bigger than 100MB
+      large-file-warning-threshold 100000000  ;; warn only when opening files bigger than 100MB
+      fill-column 80                          ;; 70 columns isn't enough
+      display-time-default-load-average nil   ;; nobody needs load average
+      display-time-24hr-format 1)
 
 ;; no bars, no gui menus
 (tool-bar-mode   -1)
@@ -889,6 +900,7 @@ _t_: toggle    _._: toggle hydra _H_: help       C-o other win no-select
              ((org-agenda-files (file-expand-wildcards (concat org-directory "*.org_archive")))))
             ;; ...other commands here
             ))
+    (define-key global-map (kbd "C-c a") 'org-agenda)
 
     ;; highlight code blocks syntax
     (setq org-src-fontify-natively  t
@@ -959,17 +971,6 @@ _t_: toggle    _._: toggle hydra _H_: help       C-o other win no-select
           ;; sets org-refile to be able to work with any org-agenda-files
           org-refile-targets (quote ((nil :maxlevel . 9)
                                      (org-agenda-files :maxlevel . 9))))
-    ;; Custom agenda views
-    (setq org-agenda-custom-commands
-          `(("u" "Unscheduled TODO items"
-             (lambda (org-match)
-               (find-file org-todo-file)
-               (org-match-sparse-tree 'todo-only "-DEADLINE={.}/!")))
-            ("t" "All TODO items"
-             (lambda (org-match)
-               (find-file org-todo-file)
-               (org-match-sparse-tree 'todo-only "/!")))
-            ("a" "Agenda" agenda)))
 
     ;; Start on the current day, not Monday.
     (setq org-agenda-start-on-weekday nil)
