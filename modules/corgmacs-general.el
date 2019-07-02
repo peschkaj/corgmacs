9d58f6fd3a71a7c52275d7796c57956d9f1539f2
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2019-07-02 07:11:45 -0700

    Ensures utf-8

--- modules/corgmacs-general.el
+++ modules/corgmacs-general.el
@@ -64,12 +64,16 @@
 (global-auto-revert-mode t)
 
 ;; make sure that UTF-8 is used everywhere.
+(setq utf-translate-cjk-mode nil)    ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
 (set-terminal-coding-system  'utf-8)
 (set-keyboard-coding-system  'utf-8)
 (set-language-environment    'utf-8)
-(set-selection-coding-system 'utf-8)
 (setq locale-coding-system   'utf-8)
 (prefer-coding-system        'utf-8)
+(set-selection-coding-system
+ (if (eq system-type 'windows-nt)
+     'utf-16-le  ;; https://rufflewind.com/2014-07-20/pasting-unicode-in-emacs-on-windows
+   'utf-8))
 (set-input-method nil)
 
 ;; always indent with spaces... except Makefiles
